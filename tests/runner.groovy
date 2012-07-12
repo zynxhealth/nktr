37e6f7a740b79be840e59b6f7a7212b43ea893cf
GalenPair <galenpair@zynx.com>
2012-07-12 15:27:00 -0700

    "Irina/Kyle/Alex - fixed a bug which prevented getting 500 errors when resources aren't available"

--- tests/runner.groovy
+++ tests/runner.groovy
@@ -4,21 +4,35 @@ import static org.junit.Assert.assertEquals
 class CoreDataTests {
     @Test
     void testGetNameEndpoint() {
-        def space = 'urn:zynx:nktr:testmoduleB'
+        def space = 'urn:zynx:nktr:testmoduleA'
+        def uri = 'active:getName2+name@John'
 
-        def uri = 'active:getName+name@John'
-
-        String request = generateRequest(space, uri)
-        String results = request.toURL().getText()
+        String results = callNKResource(space, uri)
 
         assertEquals('Say hello to John', results)
     }
+
+
+
+
+
+
+
+    private String callNKResource(String space, String uri) {
+        String request = generateRequest(space, uri)
+        String results = request.toURL().getText()
+        results
+    }
+
+
+
 
     def generateRequest(space, uri) {
         def encodedSpace = java.net.URLEncoder.encode(space)
         def doublyEncodedSpace = java.net.URLEncoder.encode(encodedSpace)
         def encodedUri = java.net.URLEncoder.encode(uri)
+        def doublyEncodedUri = java.net.URLEncoder.encode(encodedUri)
 
-        return "http://localhost:8080/nktr/run/$encodedUri/$doublyEncodedSpace"
+        return "http://localhost:8080/nktr/run/$doublyEncodedUri/$doublyEncodedSpace"
     }
 }
